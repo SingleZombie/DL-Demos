52624a98f8b8d684586ba4a509b0d8172e026f4e
Yifan Zhou <singlezombie@163.com>
2022-07-14 23:25:41 +0800

    tf cnn

---
+++ dldemos/BasicCNN/dataset.py
@@ -0,0 +1,49 @@
+import os
+from typing import Tuple
+
+import cv2
+import numpy as np
+
+
+def load_set(data_path: str, cnt: int, img_shape):
+    cat_dirs = sorted(os.listdir(os.path.join(data_path, 'cats')))
+    dog_dirs = sorted(os.listdir(os.path.join(data_path, 'dogs')))
+    images = []
+    for i, cat_dir in enumerate(cat_dirs):
+        if i >= cnt:
+            break
+        name = os.path.join(data_path, 'cats', cat_dir)
+        cat = cv2.imread(name)
+        images.append(cat)
+
+    for i, dog_dir in enumerate(dog_dirs):
+        if i >= cnt:
+            break
+        name = os.path.join(data_path, 'dogs', dog_dir)
+        dog = cv2.imread(name)
+        images.append(dog)
+
+    for i in range(len(images)):
+        images[i] = cv2.resize(images[i], img_shape)
+        images[i] = images[i].astype(np.float32) / 255.0
+
+    return np.array(images)
+
+
+def get_cat_set(
+    data_root: str,
+    img_shape: Tuple[int, int] = (224, 224),
+    train_size=1000,
+    test_size=200,
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+
+    train_X = load_set(os.path.join(data_root, 'training_set'), train_size,
+                       img_shape)
+    test_X = load_set(os.path.join(data_root, 'test_set'), test_size,
+                      img_shape)
+
+    train_Y = np.array([1] * train_size + [0] * train_size)
+    test_Y = np.array([1] * test_size + [0] * test_size)
+
+    return train_X, np.expand_dims(train_Y,
+                                   1), test_X, np.expand_dims(test_Y, 1)
---
+++ dldemos/BasicCNN/tf_main.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+
+from dldemos.BasicCNN.dataset import get_cat_set
+
+
+def init_model(input_shape=(224, 224, 3)):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(16, 11, (3, 3), input_shape=input_shape),
+        tf.keras.layers.BatchNormalization(3),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.MaxPool2D(),
+        tf.keras.layers.Conv2D(32, 5),
+        tf.keras.layers.BatchNormalization(3),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.MaxPool2D(),
+        tf.keras.layers.Conv2D(64, 3, padding='same'),
+        tf.keras.layers.BatchNormalization(3),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.Conv2D(64, 3),
+        tf.keras.layers.BatchNormalization(3),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.MaxPool2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(2048, 'relu'),
+        tf.keras.layers.Dense(1, 'sigmoid')
+    ])
+
+    model.summary()
+
+    return model
+
+
+def main():
+    train_X, train_Y, test_X, test_Y = get_cat_set(
+        'dldemos/LogisticRegression/data/archive/dataset', train_size=1500)
+    print(train_X.shape)  # (m, 224, 224, 3)
+    print(train_Y.shape)  # (m , 1)
+
+    model = init_model()
+    model.compile(optimizer='adam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+    model.fit(train_X, train_Y, epochs=20, batch_size=16)
+    model.evaluate(test_X, test_Y)
+
+
+if __name__ == '__main__':
+    main()
